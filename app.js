43fd70c786143988ef3c318a1a2a5b9a6ef1c22f
Juan Sebastian Paruma Chaparro <129807361+sebastian-paruma@users.noreply.github.com>
2025-03-18 19:37:02 -0500

    Declarado array 'amigos' para almacenar nombres

--- app.js
+++ app.js
@@ -1,1 +1,5 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+// Archivo: app.js
+// Descripción: Script principal para la lógica del Amigo Secreto
+
+// Declaración del array para almacenar los nombres de los amigos
+let amigos = []; // Almacena los nombres ingresados por el usuario
